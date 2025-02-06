7ce733856039cffe760dc85a42436ed93862d36d
Allisonhou98 <154296395+Allisonhou98@users.noreply.github.com>
2025-02-06 10:34:00 -0500

    frend end project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store';
+// ... other imports
+import * as sessionActions from './store/session'; // <-- ADD THIS LIN
+// frontend/src/main.jsx
+
+// ... other imports
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+// ... const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
+}
+
+
+const store = configureStore();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
